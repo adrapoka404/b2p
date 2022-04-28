d8775399f46a8076d40825e67efc24c701f721a0
adrapoka404 <adrapok@gmail.com>
2022-04-28 13:38:28 -0500

    alineando

--- resources/views/components/funcion.blade.php
+++ resources/views/components/funcion.blade.php
@@ -1,6 +1,6 @@
 <div class="flex-1 w-{{ $ancho }} h-{{ $alto }} items-center text-center">
     <div class=" w-{{ $ancho }} h-{{ $alto }} mx-auto my-5">
-        <img src="{{ $img }}" alt="{{ $img }}">
+        <img src="{{ $img }}" alt="{{ $img }}" class="mx-auto">
     </div>
     <div class="text-purple-200 font-extralight text-xl mt-3 w-32 mx-auto text-center">
         {{ $title }}
