645059c8c8fd7510c0d65eed8fcebacb2e4c6a9e
adrapoka404 <adrapok@gmail.com>
2022-06-30 10:54:10 -0500

    steepone guardar en base de datos

--- app/Http/Controllers/SteepOneController.php
+++ app/Http/Controllers/SteepOneController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProject;
+use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
+
+use function GuzzleHttp\Promise\all;
 
 class SteepOneController extends Controller
 {
@@ -33,15 +39,46 @@ class SteepOneController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(request $request)
+    public function store(Request $request)
     {
-        
-        echo "<pre>";
-            print_r($request->all());
-        echo "</pre>";
-        return "redirect";
+        $request->validate([
+            'project.name' => 'required'
+        ]);
 
-        return redirect()->route('steeptwo.create');
+        $nuevo = $request->all('project');
+        $nuevo = $nuevo['project'];
+        $nuevo['options'] = json_encode($nuevo['options']);
+        $nuevo['inputs'] = json_encode($nuevo['inputs']);
+        $nuevo['origins'] = json_encode($nuevo['origins']);
+        $nuevo['debs'] = json_encode($nuevo['debs']);
+        $nuevo['personal'] = json_encode($nuevo['personal']);
+        $nuevo['mobra'] = json_encode($nuevo['mobra']);
+        $nuevo['insumos'] = json_encode($nuevo['insumos']);
+        $nuevo['equipo'] = json_encode($nuevo['equipo']);
+        $nuevo['mobiliario'] = json_encode($nuevo['mobiliario']);
+        $nuevo['construccion'] = json_encode($nuevo['construccion']);
+        $nuevo['reparaciones'] = json_encode($nuevo['reparaciones']);
+        $nuevo['arrendamiento'] = json_encode($nuevo['arrendamiento']);
+        $nuevo['inmueble'] = json_encode($nuevo['inmueble']);
+        $nuevo['tramites'] = json_encode($nuevo['tramites']);
+        $nuevo['software'] = json_encode($nuevo['software']);
+        $nuevo['otros'] = json_encode($nuevo['otros']);
+        $nuevo['porcents'] = json_encode($nuevo['porcents']);
+
+
+        $url_save = 'users/projects/' . $request->project['name'] . time();
+
+        if ($request->file('project.render'))
+            $nuevo['render'] = Storage::put($url_save, $request->file('project.render'));
+        
+        if ($request->file('project.render'))
+            $nuevo['render'] = Storage::put($url_save, $request->file('project.render'));
+
+        $nuevo['url_docs'] = $url_save;
+        $nuevo['user_id'] = Auth::user()->id;
+        $nProject = Project::create($nuevo);
+        
+        return redirect()->route('steeptwo.create', $nProject->id);
     }
 
     /**
